8ae1078dc4554ac3683cb237d5f3781303e29f68
JaySurplus <znznbest2004@gmail.com>
2015-03-16 20:31:50 -0500

    Update with cs193 lecture 5

--- cs193_L5.playground/section-1.swift
+++ cs193_L5.playground/section-1.swift
@@ -2,4 +2,31 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+var str = "CS193 Lecture 5"
+
+
+/*
+Objective-C bridge
+    1. NSString bridged to String
+    2. NSArray bridged to Array<AnyObject>
+
+*/
+
+var aString = "This is a test."
+let length = (aString as NSString).length
+
+var anArray = ["a", "b", "c"]
+var join = "-".join(anArray)
+var cJS = (anArray as NSArray).componentsJoinedByString("-")
+
+
+
+/*
+Property List
+*/
+//let defauls = NSUserDefaults.standardUserDefaults()
+//let plist : AnyObject = defauls.objectForKey(String)
+
+/*
+Views
+*/
