cb9f93fd3c20d6fed5fc8212f59796aca6f3e370
JaySurplus <znznbest2004@gmail.com>
2015-03-20 16:21:37 -0500

    Methods chapter in official tutorial Done.

--- methods.playground/section-1.swift
+++ methods.playground/section-1.swift
@@ -7,24 +7,24 @@ var str = "Method"
 class Counter {
     var count = 0
     func increment() {
-        count++
+        self.count++
     }
     
     func incrementBy(#amount: Int) {
-        count += amount
+        self.count += amount
     }
     
     func currentCount() -> Int {
-        return count
+        return self.count
     }
     
     func reset() ->Bool {
-        count = 0
-        return count == 0
+        self.count = 0
+        return self.count == 0
     }
     
-    func incrementByMulti(amount: Int , numberOfTimes: Int) {
-        count += amount * numberOfTimes
+    func incrementByMulti(amount: Int , _ numberOfTimes: Int) {
+        self.count += amount * numberOfTimes
     }
 }
 
@@ -36,4 +36,62 @@ aCounter.incrementBy(amount: 5)
 println(aCounter.currentCount())
 aCounter.reset()
 println(aCounter.currentCount())
-aCounter.incrementByMulti(5,  numberOfTimes:10)
+aCounter.incrementByMulti(5,  10)
+
+
+
+struct Point {
+    var x = 0.0 , y = 0.0
+    func isToTheRightOfX(x : Double) -> Bool {
+        return self.x > x
+    }
+    
+    mutating func moveByX(deltaX: Double , y deltaY: Double) {
+        self.x += deltaX
+        self.y += deltaY
+    }
+    
+}
+
+
+
+
+enum TriStateSwitch {
+    case off, low, high
+    mutating func next() {
+        switch self {
+        case off:
+            self = low
+        case low:
+            self = high
+        case high:
+            self = off
+        }
+    }
+}
+
+var TSW = TriStateSwitch.low
+println(TSW.next())
+
+
+//Type Methods
+class typeMethods {
+    class func test(){
+
+    }
+}
+
+
+println(typeMethods.test())
+
+
+
+class Player {
+    var playerName:String
+    
+    init(name: String) {
+        playerName = name
+    }
+}
+var aPlayer = Player(name: "郭惹")
+
