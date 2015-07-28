c54fc73304c59dda40e006590aed4671dab3b235
JaySurplus <znznbest2004@gmail.com>
2015-07-27 23:41:07 -0500

    Starting initialization

--- initialization.playground/Contents.swift
+++ initialization.playground/Contents.swift
@@ -5,3 +5,29 @@ import UIKit
 var str = "Hello, Initialization."
 
 
+struct Fahrenheit {
+    var temperature: Double
+    init() {
+        temperature = 32.0
+    }
+}
+
+var f = Fahrenheit()
+
+print("The default temperature is \(f.temperature)° Fahrenheit.")
+
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+print(boilingPointOfWater.temperatureInCelsius)
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+print(freezingPointOfWater.temperatureInCelsius)
